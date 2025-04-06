29cf29f47043cefda4248768574b85b84f4595ef
Victória Amaral <ttoiav.aamaral@gmail.com>
2025-04-06 19:39:49 -0300

    programacao-para-web-ll(adição): realizando aula 02.

--- MeuProjetoFlask/MeuProjetoFlask.py
+++ MeuProjetoFlask/MeuProjetoFlask.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask.templating import render_template
 
 app=Flask(__name__) #Instancia com Variável que aloca diversos scripts na Flask
 
 @app.route('/') #Aplica uma função na seguinte
 def index():
-    return 'Hello World'
+    return render_template('index.html')
 
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
